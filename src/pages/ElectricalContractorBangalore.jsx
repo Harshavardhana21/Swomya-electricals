f9c541f6bf9adb3aba0f29d08d3a53b3d634a568
harsha <harshavardhanak21@gmail.com>
2026-05-29 21:19:29 -0700

    changes

---
+++ src/pages/ElectricalContractorBangalore.jsx
@@ -0,0 +1,14 @@
+function ElectricalContractorBangalore() {
+  return (
+    <div>
+      <h1>Electrical Contractor in Bangalore</h1>
+
+      <p>
+        Sowmya Electricals provides industrial, commercial and residential
+        electrical services across Bangalore and Karnataka.
+      </p>
+    </div>
+  );
+}
+
+export default ElectricalContractorBangalore;
